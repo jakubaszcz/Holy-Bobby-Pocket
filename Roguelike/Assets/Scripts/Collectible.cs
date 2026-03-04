0ea7c0b4830292087923e40155457e0347835c39
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-03-04 21:30:25 +0100

    fix: make it toggle only once

--- Roguelike/Assets/Scripts/Collectible.cs
+++ Roguelike/Assets/Scripts/Collectible.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    private bool _collected = false;
+
     public void Collect()
     {
+        if (_collected) return;
+        _collected = true;
+
+        GameSignals.TriggerOnCurrentCollectibles(1);
         Destroy(gameObject);
     }
 }
