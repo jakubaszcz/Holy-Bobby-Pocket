c59dd9f42f30ccb86a618f84fc4ccdd10b99e62b
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-03-08 20:45:29 +0100

    feat: add a how to play button

--- Roguelike/Assets/Scripts/Menu/Interface/InterfaceButton.cs
+++ Roguelike/Assets/Scripts/Menu/Interface/InterfaceButton.cs
@@ -3,9 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class InterfaceButton : MonoBehaviour
 {
+    
+    [SerializeField] private GameObject panelMenu;
+    [SerializeField] private GameObject panelHTP;
+
+    private void Start()
+    {
+        panelMenu.SetActive(true);
+        panelHTP.SetActive(false);
+    }
+    
     public void Play()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void HTP()
+    {
+        panelMenu.SetActive(false);
+        panelHTP.SetActive(true);
+    }
+
+    public void Back()
+    {
+        panelMenu.SetActive(true);
+        panelHTP.SetActive(false);
     }
     
     public void Exit()
